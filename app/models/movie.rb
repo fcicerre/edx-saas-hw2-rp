1e4833fdc2e49085a3bc30209e86cc9942a26c1c
Fabio Cicerre <fcicerre@gmail.com>
2013-07-27 00:03:32 -0300

    Corrected movie.rb for group movies.rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
   def Movie.get_all_ratings
-    Movie.group(:rating).order(:rating).map do |movie|
+    Movie.select(:rating).group(:rating).order(:rating).map do |movie|
       movie.rating
     end
   end
